48b3543d25f40f4b44144ba6e53c8f8b9db47708
William Reiner <williamfilardo@gmail.com>
2026-01-24 15:12:44 -0300

    atd5

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -19,7 +19,7 @@ import ridesRoutes from './routes/rides';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 // Middlewares
 app.use(helmet());
